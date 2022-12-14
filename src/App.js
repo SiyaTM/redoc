f5e6f92359cd2fd227580c4a9792b1dbfe2c0ae3
SiyaTM <siyatmahlangu@gmail.com>
2022-12-14 15:24:00 +0200

    Logo changes to be committed2

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Dirtmaate
+          Learn React
         </a>
       </header>
     </div>
